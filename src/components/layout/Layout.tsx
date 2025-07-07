0fe030f3b1db8c1059a5e5424be9f3a1826e3d87
yecharlot <152829220+yecharlot@users.noreply.github.com>
2025-07-07 10:36:32 -0400

    Create Layout.tsx

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,23 @@
+/**
+ * Componente de layout principal que envuelve toda la aplicación
+ */
+
+import { ReactNode } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
